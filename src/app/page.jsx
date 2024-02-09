4d0e8defab1f9d3311f7c8f24ad9972371deace1
bongbui321 <hoang.bui.vh@gmail.com>
2024-02-09 17:42:40 -0500

    refactor structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,8 @@
 'use client';
 
-import LikeButton from './like-button';
-import {qdlDevice} from './qdl'
+import {qdlDevice} from "@/utils/qdl";
 
-const qdldevice = new qdlDevice(); 
+const qdldevice = new qdlDevice();
 
 function Header() {
   return <h1>Flash Qualcom device</h1>
@@ -13,7 +12,7 @@ export default function HomePage() {
   return (
   <div>
     <Header />
-    <button onClick={qdldevice.connect}>connect</button>
+    <button onClick={qdldevice.connect}>Connect</button>
   </div>
   )
 }
